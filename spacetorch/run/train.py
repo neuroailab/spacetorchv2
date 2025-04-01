688a5e67d80a61c63dab4332e0a153d578915bd9
Yash Shah <ynshah@stanford.edu>
2025-04-01 13:56:18 -0700

    update docstring

--- spacetorch/run/train.py
+++ spacetorch/run/train.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     Example usage:
     export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
     
-    python -m torch.distributed.launch --nproc_per_node=8 spacetorch/run/eval.py --config spacetorch/configs/vitb14_dinov2_imagenet/swapopt_lwx1.yaml output_dir=checkpoints/vitb14_dinov2_imagenet_swapopt_lwx1 wandb=True
+    python -m torch.distributed.launch --nproc_per_node=8 spacetorch/run/train.py --config spacetorch/configs/vitb14_dinov2_imagenet/swapopt_lwx1.yaml output_dir=checkpoints/vitb14_dinov2_imagenet_swapopt_lwx1 wandb=True
     """
     args = get_args_parser(add_help=True).parse_args()
     main(args)
